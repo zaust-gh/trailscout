36b036e0833e2fef65ed5032b93a9fc4921c579c
Carl Woodhouse <carl.woodhouse@gmail.com>
2015-03-18 16:13:14 +0000

    bare bones parts, starting on some structure

---
+++ Location/Models/LocationPart.cs
@@ -0,0 +1,15 @@
+﻿using Orchard.ContentManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Trailscout.Location.Models
+{
+    public class LocationPart : ContentPart
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Postcode { get; set; }
+    }
+}
